d81e8d0cc666ea7e54a5d90f4c9d74a02bee1a12
Tomasz Weps <tomasz.weps@kldiscovery.com>
2019-09-29 21:40:03 +0200

    csp

---
+++ WebSiteAngel/Controllers/CspController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSiteAngel.Controllers
+{
+    public class CspController : Controller
+    {
+        public IActionResult On()
+        {
+            const string contentPolicy = "default-src 'none'; " +
+                "style-src angel.local; " +
+                "script-src angel.local;";
+
+            Response.Headers.Add("Content-Security-Policy", contentPolicy);
+
+            return GenerateView("CSP is ON");
+        }
+
+        public IActionResult Off()
+        {
+            return GenerateView("CSP is Off (not set)");
+        }
+
+        private IActionResult GenerateView(string title)
+        {
+            ViewData["Title"] = "CSP";
+            return View("Index");
+        }
+    }
+}
