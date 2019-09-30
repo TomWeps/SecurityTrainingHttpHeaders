61caca42b9f31068a5b640ff9590fec5c9e2b714
Tomasz Weps <tweps@krolldiscovery.com>
2019-10-01 01:49:38 +0200

    Referrer Policy

---
+++ WebSiteAngel/Controllers/ReferrerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSiteAngel.Controllers
+{
+    public class ReferrerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DefaultReferrerPolicy()
+        {
+            ViewData["Referrer"] = "Default (not set explicitly)";
+            return View("Demo");
+        }
+
+        public IActionResult NoReferrerPolicy()
+        {
+            ViewData["Referrer"] = "no-referrer";
+            Response.Headers.Add("Referrer-Policy", "no-referrer");
+            return View("Demo");
+        }
+
+       
+    }
+}
---
+++ WebSiteDevil/Controllers/RefererController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class RefererController : Controller
+    { 
+        public IActionResult Index()
+        {
+            string referer = Request.Headers["Referer"];
+            using (var stream = new System.IO.StreamWriter(@"C:\temp\Referer", append: true))
+            {
+                stream.WriteLine($"{DateTime.Now} - referer: {referer}" );
+                stream.Flush();
+                stream.Close();
+            }
+
+            return File("images/little_devil.png", "image/png");
+        }
+    }
+}
