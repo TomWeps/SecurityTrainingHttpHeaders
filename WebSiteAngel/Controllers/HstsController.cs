1825fd544d2700aeb2dcf807bc08006568e0878c
Tomasz Weps <tweps@krolldiscovery.com>
2019-10-01 00:59:44 +0200

    Hsts

---
+++ WebSiteAngel/Controllers/HstsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSiteAngel.Controllers
+{
+    public class HstsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Demo()
+        {
+            Response.Headers.Add("Strict-Transport-Security", "max-age=180;");
+                       
+            ViewData["IsHttps"] = Request.IsHttps;
+            return View();
+        }
+
+        public IActionResult Redirect()
+        {
+            Response.Headers.Add("Strict-Transport-Security", "max-age=180;");
+            return RedirectPermanent("https://angel.local/Hsts/Demo");
+        }
+    }
+}
